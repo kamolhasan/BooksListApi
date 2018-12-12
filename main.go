6643524fe65982dd6d2dfa4a2cecdb5e26ae99ec
Kamol Hasan <kamol@appscode.com>
2018-12-12 17:06:39 +0600

    C7:master api added

--- main.go
+++ main.go
@@ -1,5 +1,156 @@
-package  main
+package main
 
-func  main()  {
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+//-----------------create data structure
+type book struct {
+	ID     int     `json:"id"`
+	Title  string  `json:"title"`
+	Price  float64 `json:"price"`
+	Author Author  `json:"author"`
+}
+
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+//-------------------declare variables
+var booklist = make(map[int]book)
+
+//Create demo DB
+func CreateDB() {
+
+	booklist[1] = book{
+		ID:    1,
+		Title: "First Book",
+		Price: 2243.23,
+		Author: Author{
+			Firstname: "Kamol",
+			Lastname: "Hasan",
+		},
+	}
+
+	booklist[2] = book{
+		ID:    2,
+		Title: "Second Book",
+		Price: 23.23,
+		Author: Author{
+			Firstname: "Masudur",
+			Lastname: "Rahman",
+		},
+	}
+
+	booklist[3] = book{
+		ID:    3,
+		Title: "Third Book",
+		Price: 243.23,
+		Author: Author{
+			Firstname: "Rez1",
+			Lastname: "t",
+		},
+	}
+}
+
+//Handle requests
+func handleRequest() {
+
+	//init router
+	r := mux.NewRouter()
+	r.HandleFunc("/books", getBooks).Methods("GET")
+	r.HandleFunc("/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/books", createBook).Methods("POST")
+	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+
+// delete a certain book and print the rest books
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params:=mux.Vars(r)
+
+	v,_:=strconv.Atoi(params["id"])
+	if _,flag:=booklist[v];flag{
+		delete(booklist,v)
+	}
+
+	json.NewEncoder(w).Encode(booklist)
+}
+
+
+
+// update an existing book with new one
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params:=mux.Vars(r)
+	v,_:=strconv.Atoi(params["id"])
+
+	var newbook book
+	_=json.NewDecoder(r.Body).Decode(&newbook)
+	booklist[v]=newbook
+
+	json.NewEncoder(w).Encode(booklist)
+}
+
+
+
+
+// insert new book info to database
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var newbook book
+	_=json.NewDecoder(r.Body).Decode(&newbook)
+    booklist[newbook.ID]=newbook
+}
+
+
+
+
+
+
+
+
+
+
+// print the info of a certain book
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	v, _ := strconv.Atoi(params["id"])
+	if value, flag := booklist[v]; flag {
+		json.NewEncoder(w).Encode(value)
+		return
+	}
+	json.NewEncoder(w).Encode(&book{})
+}
+
+
+
+
+
+
+//print all books listed on database
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(booklist)
+
+}
+
+
+
+func main() {
+
+	CreateDB()
+	handleRequest()
 
 }
